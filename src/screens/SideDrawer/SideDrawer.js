d6b358bf1faa2aef095ec3ff709e1abbaed596ea
Evans Musomi <evans.musomi@gmail.com>
2018-05-29 08:37:11 +0300

    feat: adds navigator button to toggle side drawer and drawer styling

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 class SideDrawer extends Component {
   render() {
     return (
-      <View>
+      <View
+        style={[
+          styles.container,
+          { width: Dimensions.get("window").width * 0.8 }
+        ]}
+      >
         <Text>SideDrawer</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 22,
+    backgroundColor: "white",
+    flex: 1
+  }
+});
 
 export default SideDrawer;
