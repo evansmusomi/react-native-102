61361ab2c32abb04eafa50c1d980db9d2c33be3f
Evans Musomi <evans.musomi@gmail.com>
2018-06-02 07:59:08 +0300

    chore: styles share place and splits it into multiple components

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,56 +1,13 @@
 import React, { Component } from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
 
-class PlaceInput extends Component {
-  state = {
-    placeName: ""
-  };
+import DefaultInput from "../UI/DefaultInput/DefaultInput";
 
-  placeNameChangedHandler = value => {
-    this.setState({
-      placeName: value
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-    this.props.onPlaceAdded(this.state.placeName);
-  };
-
-  render() {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.placeInput}
-          placeholder="An Awesome Place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-        />
-        <Button
-          title="Add"
-          style={styles.placeButton}
-          onPress={this.placeSubmitHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  }
-});
+const PlaceInput = props => (
+  <DefaultInput
+    placeholder="Place Name"
+    value={props.placeName}
+    onChangeText={props.onChangeText}
+  />
+);
 
 export default PlaceInput;
