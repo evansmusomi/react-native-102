e9512887746a8f27d781ddcba32fe572c1f71541
Evans Musomi <evans.musomi@gmail.com>
2018-06-07 08:10:40 +0300

    Styles Nav and Makes Place Detail Responsive (#15)
    * feat: makes place detail screen responsive
    * feat: styles (colours) navigation items

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -41,10 +41,16 @@ const startTabs = () => {
           }
         }
       ],
+      tabsStyle: {
+        tabBarSelectedButtonColor: "orange"
+      },
       drawer: {
         left: {
           screen: "awesome-places.SideDrawerScreen"
         }
+      },
+      appStyle: {
+        tabBarSelectedButtonColor: "orange"
       }
     });
   });
