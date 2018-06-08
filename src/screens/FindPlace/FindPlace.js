01291196ed8e4a37f1149083bb376cc4009f9cfb
Evans Musomi <evans.musomi@gmail.com>
2018-06-08 08:03:29 +0300

    Adds Animations to Finding Places (#16)
    * feat: adds animated find places button on find place screen
    * feat: animates the loading of the list of places

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import { connect } from "react-redux";
 
 import PlaceList from "../../components/PlaceList/PlaceList";
@@ -7,6 +13,12 @@ import PlaceList from "../../components/PlaceList/PlaceList";
 class FindPlaceScreen extends Component {
   static navigatorStyle = {
     navBarButtonColor: "orange"
+  };
+
+  state = {
+    placesLoaded: false,
+    removeAnimation: new Animated.Value(1),
+    placesAnimation: new Animated.Value(0)
   };
 
   constructor(props) {
@@ -24,6 +36,27 @@ class FindPlaceScreen extends Component {
     }
   };
 
+  placesLoadedHandler = () => {
+    Animated.timing(this.state.placesAnimation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  };
+
+  placesSearchHandler = () => {
+    Animated.timing(this.state.removeAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start(() => {
+      this.setState({
+        placesLoaded: true
+      });
+      this.placesLoadedHandler();
+    });
+  };
+
   itemSelectedHandler = key => {
     const selectedPlace = this.props.places.find(place => {
       return place.key === key;
@@ -39,16 +72,65 @@ class FindPlaceScreen extends Component {
   };
 
   render() {
+    let content = (
+      <Animated.View
+        style={{
+          opacity: this.state.removeAnimation,
+          transform: [
+            {
+              scale: this.state.removeAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [10, 1]
+              })
+            }
+          ]
+        }}
+      >
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchButtonText}>Find Places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+
+    if (this.state.placesLoaded) {
+      content = (
+        <Animated.View style={{ opacity: this.state.placesAnimation }}>
+          <PlaceList
+            places={this.props.places}
+            onItemSelected={this.itemSelectedHandler}
+          />
+        </Animated.View>
+      );
+    }
+
     return (
-      <View>
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.itemSelectedHandler}
-        />
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  searchButton: {
+    borderColor: "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
+  }
+});
 
 const mapStateToProps = state => {
   return {
