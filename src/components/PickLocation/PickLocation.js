e173354aea85b61f19ee082b5bf270a2cab7ec58
Evans Musomi <evans.musomi@gmail.com>
2018-06-15 05:09:34 +0300

    Adds Map Marker and Animates to Selected Region (#23)
    * feat: navigates to a selected area on the map
    * feat: adds a map marker at the selected location
    * feat: animates map movement

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -11,15 +11,46 @@ class PickLocation extends Component {
       longitudeDelta:
         (Dimensions.get("window").width / Dimensions.get("window").height) *
         0.0122
-    }
+    },
+    locationChosen: false
   };
+
+  pickLocationHandler = event => {
+    const coordinates = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude
+    });
+    this.setState(previousState => {
+      return {
+        focusedLocation: {
+          ...previousState.focusedLocation,
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude
+        },
+        locationChosen: true
+      };
+    });
+  };
+
   render() {
+    let marker = null;
+
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    }
+
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
           initialRegion={this.state.focusedLocation}
-        />
+          onPress={this.pickLocationHandler}
+          ref={ref => (this.map = ref)}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
           <Button title="Locate Me" onPress={() => alert("Locate me")} />
         </View>
