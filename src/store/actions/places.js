5a12e85c821af3ef7c711e34222238365052d34e
Evans Musomi <evans.musomi@gmail.com>
2018-06-20 08:16:11 +0300

    feat: saves shared place name and location on firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,26 @@
 import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch("https://awesomeplaces-1528951686629.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log(parsedResponse);
+        dispatch({
+          type: ADD_PLACE,
+          placeName: placeName,
+          location: location,
+          image: image
+        });
+      });
   };
 };
 
