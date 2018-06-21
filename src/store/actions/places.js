f3feeed1eadc2f87c847104088b4d820c429e162
Evans Musomi <evans.musomi@gmail.com>
2018-06-21 09:02:15 +0300

    Uploads and Stores Image on Firebase (#29)
    * feat: uploads and stores image on firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,14 +2,32 @@ import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
-    fetch("https://awesomeplaces-1528951686629.firebaseio.com/places.json", {
-      method: "POST",
-      body: JSON.stringify(placeData)
-    })
+    fetch(
+      "https://us-central1-awesomeplaces-1528951686629.cloudfunctions.net/storeImage",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log(parsedResponse);
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedResponse.imageUrl
+        };
+        return fetch(
+          "https://awesomeplaces-1528951686629.firebaseio.com/places.json",
+          {
+            method: "POST",
+            body: JSON.stringify(placeData)
+          }
+        );
+      })
       .catch(error => console.log(error))
       .then(response => response.json())
       .then(parsedResponse => {
