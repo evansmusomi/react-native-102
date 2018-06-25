362177e3cb1f81ce399dcdc89b0911ef9f38e39a
Evans Musomi <evans.musomi@gmail.com>
2018-06-25 07:45:48 +0300

    feat: signs up users and saves their data on firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,34 @@
 import { TRY_AUTH } from "./actionTypes";
 
 export const tryAuth = authData => {
-  return {
-    type: TRY_AUTH,
-    authData: authData
+  return dispatch => {
+    dispatch(authSignUp(authData));
+  };
+};
+
+export const authSignUp = authData => {
+  return dispatch => {
+    fetch(
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyASvVR4L25fpyeyAy1c48Op1aQfZ1Zab5s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .catch(error => {
+        console.log(error);
+        alert("Authentication failed, please try again!");
+      })
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log(parsedResponse);
+      });
   };
 };
