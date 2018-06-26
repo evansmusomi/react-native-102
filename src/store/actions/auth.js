52b9da8177bb6f36f8aa7057f4140c1fd97a2afc
Evans Musomi <evans.musomi@gmail.com>
2018-06-26 08:52:51 +0300

    Uses Env Variables and Supports Login via Firebase (#34)
    * chore: uses env file to store keys
    * feat: adds login support via firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,33 +1,32 @@
+import Config from "react-native-config";
 import { TRY_AUTH } from "./actionTypes";
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
 export const tryAuth = (authData, authMode) => {
   return dispatch => {
-    if (authMode === "login") {
-    } else {
-      dispatch(authSignUp(authData));
-    }
-  };
-};
-
-export const authSignUp = authData => {
-  return dispatch => {
     dispatch(uiStartLoading());
-    fetch(
-      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyASvVR4L25fpyeyAy1c48Op1aQfZ1Zab5s",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: authData.email,
-          password: authData.password,
-          returnSecureToken: true
-        }),
-        headers: {
-          "Content-Type": "application/json"
-        }
+    let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
+      Config.FIREBASE_API_KEY
+    }`;
+
+    if (authMode === "signup") {
+      url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+        Config.FIREBASE_API_KEY
+      }`;
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
       }
-    )
+    })
       .catch(error => {
         dispatch(uiStopLoading());
         console.log(error);
