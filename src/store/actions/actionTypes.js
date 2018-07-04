d73e6bee9fda4e46787f3e92783044c292212abe
Evans Musomi <evans.musomi@gmail.com>
2018-07-04 08:21:59 +0300

    feat: redirects to find places screen after sharing a place and ensures all places are shown always

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,5 +1,7 @@
 export const SET_PLACES = "SET_PLACES";
 export const SET_NEW_PLACE = "SET_NEW_PLACE";
+export const PLACE_ADDED = "PLACE_ADDED";
+export const START_ADD_PLACE = "START_ADD_PLACE";
 export const REMOVE_PLACE = "REMOVE_PLACE";
 export const TRY_AUTH = "TRY_AUTH";
 
